2b7f63337b95bccf9d2528d4a84083d1dad25072
3dSpace116 <mirskam@gmail.com>
2024-03-21 23:58:26 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Задача 1
+
+        int totalExpenses = 0; //суммарное значение трат за месяц
+        int[] expenses = {500, 7200, 900, 1800, 2000};
+        for (int i : expenses) {  // можно через обычный for (int i = 0; i < expenses.length; i++), но я захотел попробовать for each
+            totalExpenses = totalExpenses + i; //тут ждал подвох, если сделать+ expenses[i], то не работает, как оказалось, внутри for each переменная i принимает значения ячейки
+        }
+        System.out.println("«Сумма трат за месяц составила " + totalExpenses + " рублей».");
+
+        //Задача 2
+
+        int minExpenses=0;
+        int maxExpenses=0;
+
     }
 }
